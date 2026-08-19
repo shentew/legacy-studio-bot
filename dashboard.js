134839f0bd4d232147540eeb4e27ec015daab1c5
Shentew <brianpadre14@gmail.com>
2026-08-19 03:19:24 -0300

    Update dashboard.js

--- dashboard.js
+++ dashboard.js
@@ -7,7 +7,7 @@ module.exports = (client) => {
     const { AttachmentBuilder, EmbedBuilder, ActionRowBuilder, StringSelectMenuBuilder } = require('discord.js');
 
     const app = express();
-    const PORT = process.env.PORT || 3000;
+    const PORT = 26212;
 
     app.use(express.urlencoded({ extended: true }));
     app.use(express.json());
