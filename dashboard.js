27ad5fc9bac2e060cc411207241606229b8ba655
Shentew <brianpadre14@gmail.com>
2026-08-18 22:53:17 -0300

    puerto

--- dashboard.js
+++ dashboard.js
@@ -7,6 +7,7 @@ module.exports = (client) => {
 
     const app = express();
     const PORT = process.env.PORT || 3000;
+    app.listen(PORT, () => console.log(`🌐 Dashboard corriendo en puerto ${PORT}`));
 
     app.use(express.urlencoded({ extended: true }));
     app.use(express.json());
